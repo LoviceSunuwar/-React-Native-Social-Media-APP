93c0f8eff1ea00173541a0e6a0df852eabe41cc7
shivamsaini485 <c0834485@mylambton.ca>
2022-12-08 18:58:47 -0500

    Notification properties

--- components/notification/Notification.js
+++ components/notification/Notification.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
 
-const Notification = (props) => {
-  const { notification } = props;
 
-  if (!notification) {
-    return <></>;
-  }
-
-  return (
-    <View style={styles.listItem}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.listItemImage} source={{ uri: notification.notificationImage }} />
-      </View>
-      <Text style={styles.listItemText}>{notification.notificationMessage}</Text>
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
   listItem: {
